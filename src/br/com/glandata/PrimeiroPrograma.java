4df4a60b2c6da839d2b91aaff9c9c2e34c3c6962
Giancarlo Leite <giancarlo.leite@outlook.com>
2022-05-18 20:22:50 -0300

    Alteração do Sysout

--- src/br/com/glandata/PrimeiroPrograma.java
+++ src/br/com/glandata/PrimeiroPrograma.java
@@ -3,7 +3,7 @@ package br.com.glandata;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		System.out.println("Primeiro Programa!");	
+		System.out.println("Olá Programa!");	
 	}
 
 }
